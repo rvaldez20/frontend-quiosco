0245a6b03e9231aace8c77aa38d8ecdf3639c18c
rvaldez20 <rvaldez20@gmail.com>
2023-12-07 19:00:16 -0600

    [ADD] se agregan las imagenes del proyecto y se empieza con la maquetación de AuthLayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom"
 
+import Logo from '../assets/img/logo.svg'
+
 export default function AuthLayout() {
   return (
-    <div>
-      AuthLayout
-    
-      <Outlet />
-    </div>
+    <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+      <img 
+        src={ Logo }
+        alt='imagen logotipo'
+        className="max-w-xs"
+      />
+
+      <div className="p-10 w-full">
+        <Outlet />
+      </div>
+    </main>
   )
 }

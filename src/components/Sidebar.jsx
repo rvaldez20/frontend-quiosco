a805d3f8ce6197a523d6176a75150cadd66a49b5
rvaldez20 <rvaldez20@gmail.com>
2023-12-09 13:43:45 -0600

    [ADD] se empieza a trabajar en el Sidebar: muestra logo y categorias

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,27 @@
 
+import { categories } from '../data/categories'
+import Logo from '../assets/img/logo.svg'
+
 export default function Sidebar() {
   return (
-   <div className="md:w-72">
-      Sidebar
-   </div>
+   <aside className="md:w-72">
+      
+      <div className="p-4">
+         <img 
+            src={ Logo } 
+            alt="imagen logotipo" 
+            className='w-40'
+         />
+      </div>
+
+      <div className='mt-10'>
+         {categories.map((category) => (            
+            <p key={category.id}>{category.nombre}</p>
+         ))}
+      </div>
+
+
+
+   </aside>
   )
 }
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
+import Resumen from '../components/Resumen'
+import Sidebar from '../components/Sidebar'
+
 export default function Layout() {
   return (
-    <div>
-      Layout
+    <div className='md:flex'>
+      <Sidebar />
 
-      <Outlet /> 
+      <main className='flex-1'>
+        <Outlet /> 
+      </main>
 
+      <Resumen />       
     </div>
   )
 }

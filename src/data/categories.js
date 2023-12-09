e32b2a37646c7a8ec4ca62cc4956eda88ba8ab84
rvaldez20 <rvaldez20@gmail.com>
2023-12-09 13:42:39 -0600

    [ADD] se creauan data para las categorias mientras se implementa el backend

---
+++ src/data/categories.js
@@ -0,0 +1,36 @@
+const categories = [
+   {
+      icono: "cafe",
+      nombre: "Café",
+      id: 1,
+   },
+   {
+      icono: "hamburguesa",
+      nombre: "Hamburguesas",
+      id: 2,
+   },
+   {
+      icono: "pizzas",
+      nombre: "Pizzas",
+      id: 3,
+   },
+   {
+      icono: "donas",
+      nombre: "Donas",
+      id: 4,
+   },
+   {
+      icono: "pastel",
+      nombre: "Pasteles",
+      id: 5,
+   },
+   {
+      icono: "galletas",
+      nombre: "Galletas",
+      id: 6,
+   },
+]
+
+export {
+   categories
+}
